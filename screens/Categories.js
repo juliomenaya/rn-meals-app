f9a6833b6d0e6dbf98a58f390ef3202068ce43fc
juliomenaya <juliomenaya@gmail.com>
2021-02-15 00:56:22 -0600

    rendering category meals list

--- screens/Categories.js
+++ screens/Categories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
 
--- screens/CategoryMeals.js
+++ screens/CategoryMeals.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, FlatList } from 'react-native';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
 
 
 const CategoryMeals = props => {
+
+    const category = props.navigation.getParam('category');
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(category.id) >= 0)
+    const renderMealItem = itemData => {
+        return (
+            <View>
+                <Text>{itemData.item.title}</Text>
+            </View>
+        )
+    };
+
 
     const goToMealDetail = () => {
         props.navigation.navigate('MealDetail');
@@ -10,8 +22,7 @@ const CategoryMeals = props => {
 
     return (
         <View style={styles.screen}>
-            <Text>The category meal screen</Text>
-            <Button title='Give me details!' onPress={goToMealDetail} />
+            <FlatList data={displayedMeals} keyExtractor={(item, index) => item.id} renderItem={renderMealItem}/>
         </View>
     );
 };
