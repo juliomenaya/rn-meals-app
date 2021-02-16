a3f91c9f8222676cd5394136f1bf686bbb02dbe9
juliomenaya <juliomenaya@gmail.com>
2021-02-15 20:01:52 -0600

    materialbotton for android devices

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,11 +1,17 @@
 
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation';
 import Categories from '../screens/Categories';
 import CategoryMeals from '../screens/CategoryMeals';
 import MealDetail from '../screens/MealDetail';
 import { Platform } from 'react-native';
 import Colors from '../constants/Colors';
+import Favorites from '../screens/Favorites';
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+
 
 const MealsNavigator = createStackNavigator(
     {
@@ -28,4 +34,44 @@ const MealsNavigator = createStackNavigator(
     }
 });
 
-export default createAppContainer(MealsNavigator);
+const tabScreenConfig = {
+    Meals: {
+        screen: MealsNavigator, 
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />
+            },
+            tabBarColor: Colors.primary
+        }
+    },
+    Favorites: {
+        screen: Favorites,
+        navigationOptions: {
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
+            },
+            tabBarColor: Colors.secondary
+            // tabBarLabel: 'Favorites! prro'
+        }
+    }
+};
+
+const MealsFavTabNavigator = 
+    Platform.OS === 'android' 
+        ? createMaterialBottomTabNavigator(
+            tabScreenConfig,
+            {
+                activeColor: 'white',
+                shifting: true
+            }
+        ) 
+        : createBottomTabNavigator(
+            tabScreenConfig,
+            {
+                tabBarOptions: {
+                    activeTintColor: Colors.secondary
+                }
+            }
+);
+
+export default createAppContainer(MealsFavTabNavigator);
--- screens/Favorites.js
+++ screens/Favorites.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const Favorites = () => {
     return (
         <View style={styles.screen}>
-            <Text></Text>
+            <Text>Favorites prro</Text>
         </View>
     );
 };
