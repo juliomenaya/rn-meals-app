9931db32560f8ca567b84d25de8c97cb6650977e
juliomenaya <juliomenaya@gmail.com>
2021-02-18 19:50:02 -0600

    meal detail

--- screens/MealDetail.js
+++ screens/MealDetail.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
+
+const ListItem = props => {
+    return (
+        <View style={styles.listItem}>
+            <DefaultText>{props.children}</DefaultText>
+        </View>
+    );
+};
 
 
 const MealDetail = props => {
     const selectedMeal = props.navigation.getParam('meal');;
     return (
-        <View style={styles.screen}>
-            <Text>{selectedMeal.title}</Text>
-        </View>
+        <ScrollView>
+            <Image source={{uri: selectedMeal.imageUrl}} style={styles.image}/>
+            <View style={styles.details}>
+                <DefaultText>{selectedMeal.duration}m</DefaultText>
+                <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+                <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+            </View>
+            <Text style={styles.title}>Ingredients</Text>
+            {selectedMeal.ingredients.map((ingredient, idx) => {
+                return (
+                    <ListItem key={idx}>{ingredient}</ListItem>
+                )
+            })}
+            <Text style={styles.title}>Steps</Text>
+            {selectedMeal.steps.map((step, idx) => {
+                return (
+                    <ListItem key={idx}>{step}</ListItem>
+                )
+            })}
+        </ScrollView>
     );
 };
 
@@ -20,7 +46,7 @@ MealDetail.navigationOptions = navigationData => {
         headerRight: () => {
             return (
                 <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                    <Item title='Favorite' iconName='ios-star' onPress={() => console.log('Marked as favorite')}/>
+                    <Item title='Favorite' iconName='ios-star' onPress={() => console.log('Marked as favorite')} />
                 </HeaderButtons>
             )
         }
@@ -30,9 +56,25 @@ MealDetail.navigationOptions = navigationData => {
 export default MealDetail;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    details: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-around'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 10
     }
 });
